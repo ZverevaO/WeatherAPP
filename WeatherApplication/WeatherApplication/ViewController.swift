fecb09f7fc5984212f01e974aadd5b25a15c489c
Oksana_Zvereva <zvereva.oksana@gmail.com>
2020-03-28 14:17:35 +0500

    переходы, обработка не вернно введенного пароля

--- WeatherApplication/WeatherApplication/ViewController.swift
+++ WeatherApplication/WeatherApplication/ViewController.swift
@@ -16,31 +16,21 @@ class ViewController: UIViewController {
    
     @IBOutlet weak var scrollView: UIScrollView!
     
-    @IBAction func onClickButton(_ sender: UIButton) {
-        
-        //        guard let login = loginTextFild.text, let password = passworTextField.text else {return}
-        
-        let login = loginTextField.text!
-        let password = passwordTextField.text!
-        
-        print (login + " " + password)
-    }
-    
-    
     override func viewDidLoad() {
-           super.viewDidLoad()
-           
-           // Do any additional setup after loading the view.
-           
-           let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-           view.addGestureRecognizer(hideAction)
-           
-       }
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(hideAction)
+        
+    }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        // подписываемся на уведомления
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
@@ -51,6 +41,8 @@ class ViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
+        // отписываемся от уведомлений
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
@@ -81,5 +73,44 @@ class ViewController: UIViewController {
     }
     
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        
+        switch identifier {
+        case "loginSegue":
+            
+            let isAuth = login()
+            
+            if !isAuth {
+                showErrorAlert()
+            }
+            
+            return isAuth
+        default:
+            return true
+        }
+    }
+    
+    func login () -> Bool {
+        // функция проверки учетных данных пользователя
+        let login = loginTextField.text!
+        let password = passwordTextField.text!
+        return login == "admin" && password == "123456"
+        
+    }
+    
+    func showErrorAlert() {
+        // вывод сообщения для пользователя
+        // Создаем контроллер
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: "Введены неверные данные пользователя",
+            preferredStyle: .alert)
+        // Создаем кнопку для UIAlertController
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        // Добавляем кнопку на UIAlertController
+        alert.addAction(action)
+        // Показываем UIAlertController
+        present(alert, animated: true)
+    }
 }
 

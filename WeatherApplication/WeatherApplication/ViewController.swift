df673031334a4455bf40871e4e2a128456ec8758
Oksana_Zvereva <zvereva.oksana@gmail.com>
2020-04-19 15:58:24 +0500

    изменения

--- WeatherApplication/WeatherApplication/ViewController.swift
+++ WeatherApplication/WeatherApplication/ViewController.swift
@@ -19,7 +19,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     
     
-    @IBOutlet var loginView: UIView!
+    @IBOutlet var loginVloginTextFieldiew: UIView!
+    
+    @IBOutlet weak var mainTitle: UILabel!
+    
+    @IBOutlet weak var loginTitileView: UILabel!
+    
+    @IBOutlet weak var passwordTitleView: UILabel!
+    
+    
+    var interactiveAnimator: UIViewPropertyAnimator!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +39,12 @@ class ViewController: UIViewController {
         let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(hideAction)
         
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        self.view.addGestureRecognizer(recognizer)
+
+        
     }
+    
     
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,6 +58,16 @@ class ViewController: UIViewController {
         
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateTitlesAppearing()
+        animateTitleAppearing()
+        animateFieldsAppearing()
+        animateAuthButton ()
+        
+    }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -121,7 +146,124 @@ class ViewController: UIViewController {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-//       loginButton.layer.cornerRadius = 3
+        //       loginButton.layer.cornerRadius = 3
+    }
+    
+    func animateTitlesAppearing() {
+//        let offset = view.bounds.width
+//        loginTitileView.transform = CGAffineTransform(translationX: -offset, y: 0)
+//        passwordTitleView.transform = CGAffineTransform(translationX: offset, y: 0)
+//
+//        UIView.animate(withDuration: 1,
+//                       delay: 1,
+//                       options: .curveEaseOut,
+//                       animations: {
+//                        self.loginTitileView.transform = .identity
+//                        self.passwordTitleView.transform = .identity
+//        },
+//                       completion: nil)
+        let offset = abs(self.loginTitileView.frame.midY - self.passwordTitleView.frame.midY)
+                
+        self.loginTitileView.transform = CGAffineTransform(translationX: 0, y: offset)
+        self.passwordTitleView.transform = CGAffineTransform(translationX: 0, y: -offset)
+        UIView.animateKeyframes(withDuration: 1,
+                                delay: 1,
+                                options: .calculationModeCubicPaced,
+                                animations: {
+                                    UIView.addKeyframe(withRelativeStartTime: 0,
+                                                       relativeDuration: 0.5,
+                                                       animations: {
+             self.loginTitileView.transform = CGAffineTransform(translationX: 150, y: 50)
+             self.passwordTitleView.transform = CGAffineTransform(translationX: -150, y: -50)
+                                    })
+                                    UIView.addKeyframe(withRelativeStartTime: 0.5,
+                                                       relativeDuration: 0.5,
+                                                       animations: {
+             self.loginTitileView.transform = .identity
+             self.passwordTitleView.transform = .identity
+                                    })
+        }, completion: nil)
+    }
+    
+    func animateTitleAppearing() {
+//        self.mainTitle.transform = CGAffineTransform(translationX: 0,
+//                                                     y: -self.view.bounds.height/2)
+//
+//        UIView.animate(withDuration: 1,
+//                       delay: 1,
+//                       usingSpringWithDamping: 0.5,
+//                       initialSpringVelocity: 0,
+//                       options: .curveEaseOut,
+//                       animations: {
+//                        self.mainTitle.transform = .identity
+//        },
+//                       completion: nil)
+        self.mainTitle.transform = CGAffineTransform(translationX: 0, y: -self.view.bounds.height / 2)
+
+        let animator = UIViewPropertyAnimator(duration: 1,
+                                              dampingRatio: 0.5,
+                                              animations: {
+                                                  self.mainTitle.transform = .identity
+        })
+
+        animator.startAnimation(afterDelay: 1)
+    }
+    
+    
+    func animateFieldsAppearing() {
+        let fadeInAnimation = CABasicAnimation(keyPath: "opacity")
+        fadeInAnimation.fromValue = 0
+        fadeInAnimation.toValue = 1
+        fadeInAnimation.duration = 1
+        fadeInAnimation.beginTime = CACurrentMediaTime() + 1
+        fadeInAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        fadeInAnimation.fillMode = CAMediaTimingFillMode.backwards
+        
+        self.loginTextField.layer.add(fadeInAnimation, forKey: nil)
+        self.passwordTextField.layer.add(fadeInAnimation, forKey: nil)
+    }
+    
+    
+    func animateAuthButton() {
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.stiffness = 200
+        animation.mass = 2
+        animation.duration = 2
+        animation.beginTime = CACurrentMediaTime() + 1
+        animation.fillMode = CAMediaTimingFillMode.backwards
+        
+        self.loginButton.layer.add(animation, forKey: nil)
+    }
+    
+    
+    @objc func onPan(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            interactiveAnimator?.startAnimation()
+            
+            interactiveAnimator = UIViewPropertyAnimator(duration: 0.5,
+                                                         dampingRatio: 0.5,
+                                                         animations: {
+                self.loginButton.transform = CGAffineTransform(translationX: 0,
+                                                              y: 150)
+            })
+            
+            interactiveAnimator.pauseAnimation()
+        case .changed:
+            let translation = recognizer.translation(in: self.view)
+            interactiveAnimator.fractionComplete = translation.y / 100
+        case .ended:
+            interactiveAnimator.stopAnimation(true)
+            
+            interactiveAnimator.addAnimations {
+                self.loginButton.transform = .identity
+            }
+            
+            interactiveAnimator.startAnimation()
+        default: return
+        }
     }
 
 }
